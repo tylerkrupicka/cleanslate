9f265377a412f60a7daae1af7e6ce9d88656f90a
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-08-01 23:14:47 -0400

    Formatted and removed iframe

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
- $( document ).ready(function() {
-    console.log("im tiny rick");
+$(document).ready(function() {
+    console.log("Clean Slate Ready..");
     var editButtons = $("a[data-hover='tooltip'][data-tooltip-content='Edit'][rel='toggle']");
-    for(var i = 0; i < editButtons.length; i++){
+    for (var i = 0; i < editButtons.length; i++) {
         editButtons[i].click();
     }
     var buttons = $("a[role='menuitem']");
-    for(i = 0; i < buttons.length; i++){
+    for (i = 0; i < buttons.length; i++) {
         var ajax = $(buttons[i]).attr("ajaxify");
-        if(ajax && ajax.indexOf("comment") != -1){
+        if (ajax && ajax.indexOf("comment") != -1) {
             console.log("comment");
             console.log(buttons[i]);
-        } else if(ajax && ajax.indexOf("unlike") != -1){
+        } else if (ajax && ajax.indexOf("unlike") != -1) {
             console.log("like");
             console.log(buttons[i]);
             //buttons[i].click();
             break;
         } else {
-         console.log("something else");
-         console.log(buttons[i]);
+            console.log("something else");
+            console.log(buttons[i]);
         }
     }
-    });
+});
