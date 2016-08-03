90b4af038bd153ce016025a2fc7ec8e44596f142
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-08-02 22:23:23 -0400

    refactored and added scroll to bottom

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-console.log("Clean Slate Ready..");
-var editButtons = $("a[data-hover='tooltip'][data-tooltip-content='Edit'][rel='toggle']");
-for (var i = 0; i < editButtons.length; i++) {
-    editButtons[i].click();
+function run(){
+    console.log("Clean Slate Running..");
+    clickEditButtons();
+    clickActionButtons();
+    scrollToBottom();
+    console.log("I'M TINY RICK");
 }
-var buttons = $("a[role='menuitem']");
-for (i = 0; i < buttons.length; i++) {
-    var ajax = $(buttons[i]).attr("ajaxify");
-    if (ajax && ajax.indexOf("comment") != -1) {
-        console.log("comment");
-        console.log(buttons[i]);
-    } else if (ajax && ajax.indexOf("unlike") != -1) {
-        console.log("like");
-        console.log(buttons[i]);
-        //buttons[i].click();
-    } else {
-        console.log("something else");
-        console.log(buttons[i]);
+
+function clickEditButtons(){
+    /*
+        Click the edit buttons so that the menuItems get loaded.
+    */
+    var editButtons = $("a[data-hover='tooltip'][data-tooltip-content='Edit'][rel='toggle']");
+    console.log("Clicking " + editButtons.length + " edit buttons.");
+    for (var i = 0; i < editButtons.length; i++) {
+        editButtons[i].click();
     }
 }
+
+function clickActionButtons(){
+    /*
+        Click the menu buttons that will actually delete content.
+    */
+    var buttons = $("a[role='menuitem']");
+    for (i = 0; i < buttons.length; i++) {
+        var ajax = $(buttons[i]).attr("ajaxify");
+        if (ajax && ajax.indexOf("comment") != -1) {
+            console.log("Found comment.. ");
+        } else if (ajax && ajax.indexOf("unlike") != -1) {
+            console.log("Found like.. ");
+            //buttons[i].click();
+        } else {
+            console.log("Found something else.. ");
+        }
+    }
+}
+
+function scrollToBottom(){
+    /* Scroll to the bottom of the page */
+    $('html, body').scrollTop($(document).height());
+}
+
+run();
